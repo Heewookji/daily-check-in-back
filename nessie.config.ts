8f138dab09a86a5069c3b76da19ee578e34f1ee1
heewookji <wlgmldnr@daum.net>
2021-12-18 13:44:39 +0900

    chore: mysql to postgresql

--- nessie.config.ts
+++ nessie.config.ts
@@ -1,8 +1,10 @@
-import {NessieConfig, ClientMySQL} from "./deps.ts";
+import {
+    ClientPostgreSQL,
+    NessieConfig,
+} from "./deps.ts";
 import { config } from "./src/config.ts";
 
-
-const client = new ClientMySQL(config.dbConfig);
+const client = new ClientPostgreSQL(config.nessieDBConfig);
 
 /** This is the final config object */
 const configPg: NessieConfig = {
